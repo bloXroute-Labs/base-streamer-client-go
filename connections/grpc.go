bae34b2f712c373995664f7aafa408e5fbc8ef48
ron-elmekiesse-bloxroute <ron.elmekiesse@bloxroute.com>
2025-03-24 12:17:04 +0200

    Add connections folder

---
+++ connections/grpc.go
@@ -0,0 +1,23 @@
+package connections
+
+import (
+	"fmt"
+	"io"
+
+	"google.golang.org/grpc"
+)
+
+func GRPCStream[T any](stream grpc.ClientStream, input string) Streamer[*T] {
+	var generator Streamer[*T] = func() (*T, error) {
+		m := new(T)
+		err := stream.RecvMsg(m)
+		if err == io.EOF {
+			return nil, fmt.Errorf("stream for input %s ended successfully", input)
+		} else if err != nil {
+			return nil, err
+		}
+		return m, nil
+	}
+
+	return generator
+}
