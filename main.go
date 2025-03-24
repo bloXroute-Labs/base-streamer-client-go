8fa335361859b67a1ced687fbd93d488befada8c
ron-elmekiesse-bloxroute <ron.elmekiesse@bloxroute.com>
2025-03-24 12:41:18 +0200

    Add main

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/bloXroute-Labs/base-streamer-client-go/examples"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	_ = godotenv.Load()
+
+	err := examples.ListenForNewBlocks(10)
+	if err != nil {
+		panic(err)
+	}
+}
