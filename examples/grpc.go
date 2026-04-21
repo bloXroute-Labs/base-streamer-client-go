95f0e7a243208badbcbc4b8620b96b100b7d97fb
ron-elmekiesse-bloxroute <ron.elmekiesse@bloxroute.com>
2026-04-21 12:31:30 +0300

    remove bdn blocks stream

--- examples/grpc.go
+++ examples/grpc.go
@@ -7,47 +7,32 @@ import (
 
 	"github.com/bloXroute-Labs/base-streamer-client-go/provider"
 	streamerapi "github.com/bloXroute-Labs/base-streamer-proto/streamer_api"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/rlp"
 )
 
-func ListenForBdnBlocks(numberOfBlocks uint64) error {
+func ListenForParsedBdnFlashBlocks(numberOfBlocks uint64) error {
 	grpcClient, err := provider.NewGRPCClient()
 	if err != nil {
 		return err
 	}
-	blocksChan := make(chan *streamerapi.GetBdnBlockStreamResponse)
+	flashblocksChan := make(chan *streamerapi.GetParsedBdnFlashBlockStreamResponse)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	stream, err := grpcClient.GetBdnBlockStream(ctx)
+	stream, err := grpcClient.GetParsedBdnFlashBlockStream(ctx)
 	if err != nil {
 		return fmt.Errorf("failed to get stream with: %v", err)
 	}
-	stream.Into(blocksChan)
+	stream.Into(flashblocksChan)
 
-	fmt.Println("waiting on blocks channel")
+	fmt.Println("waiting on parsed bdn flashblocks channel")
 	for range numberOfBlocks {
-		bdnBlock, ok := <-blocksChan
+		flashblock, ok := <-flashblocksChan
 		if !ok {
 			// channel closed
-			return fmt.Errorf("bdn blocks channel closed")
+			return fmt.Errorf("parsed bdn flashblocks channel closed")
 		}
 		updateTime := time.Now()
-
-		blockHeader := &types.Header{}
-		err := rlp.DecodeBytes(bdnBlock.BlockHeader, blockHeader)
-		if err != nil {
-			return fmt.Errorf("failed to RLP decode block header with: %v", err)
-		}
-
-		blockBody := &types.Body{}
-		err = rlp.DecodeBytes(bdnBlock.BlockBody, blockBody)
-		if err != nil {
-			return fmt.Errorf("failed to RLP decode block body with: %v", err)
-		}
-
-		fmt.Printf("bdn block: %v, %v txns at %v\n", blockHeader.Number.Uint64(), len(blockBody.Transactions), updateTime.UTC())
+		fmt.Printf("bdn flash block: %v, index: %v, at %v\n", flashblock.Metadata.BlockNumber, flashblock.Index, updateTime.UTC())
 	}
 	return nil
 }
